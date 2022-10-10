bd3bbb443ead3e3fccd178c98df88a66ee59acc2
YetiBarBar <yetibarbar@gmail.com>
2022-10-10 22:08:28 +0200

    Add solution for day 1

---
+++ src/day1.rs
@@ -0,0 +1,31 @@
+use std::collections::HashSet;
+
+fn main() {
+    let data = include_str!("../data/day1.data");
+    let res: isize = data
+        .lines()
+        .map(|line| line.trim_start_matches('+'))
+        .map(|s| s.parse::<isize>())
+        .map(Result::unwrap)
+        .sum();
+    println!("Part 1: {}", res);
+
+    let sequence: Vec<_> = data
+        .lines()
+        .map(|line| line.trim_start_matches('+'))
+        .map(|s| s.parse::<isize>())
+        .map(Result::unwrap)
+        .collect();
+
+    let mut last: isize = 0;
+    let mut set = HashSet::new();
+    for chg in sequence.iter().cycle() {
+        last += chg;
+        if set.contains(&last) {
+            println!("Part 2: {}", last);
+            break;
+        } else {
+            set.insert(last);
+        }
+    }
+}
